7b6666f7104eb3e1704e5187100dccd31cb3f39b
Hesam N <AnonCODER@tutanota.com>
2021-12-14 16:55:39 +0330

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from SearchSpider import *
-
-url = PreURL('https://pypi.org', 'https', 'https://pypi.org', '/search', 'q', 'numpy', 'span', get_by_class_title='package-snippet__name',
-get_by_class_links='package-snippet__version')
-
-URL = url.URL()
